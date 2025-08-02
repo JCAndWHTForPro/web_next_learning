220ce26f27036a0eeb0470640544bd9e59c6e4df
jicheng <jichenglovegloria@gmail.com>
2025-08-03 01:45:39 +0800

    tijiao

--- basic_syntax/var_type_init.go
+++ basic_syntax/var_type_init.go
@@ -1,6 +1,7 @@
 /*变量的声明与初始化*/
 package main
 
+//包引入了也必须要被使用，否则会编译报错
 import "fmt"
 
 var x, y int
@@ -9,6 +10,7 @@ var ( // 这种因式分解关键字的写法一般用于声明全局变量
 	b bool
 )
 
+// 全局变量可以被声明但是不被使用
 var c, d int = 1, 2
 var e, f = 123, "hello"
 
@@ -16,6 +18,7 @@ var e, f = 123, "hello"
 //g, h := 123, "hello"
 
 func main() {
+	//局部变量声明了必须被使用
 	g, h := 123, "hello"
-	fmt.Println(x, y, a, b, c, d, e, f, g, h)
+	fmt.Println(x, y, a, b, e, f, g, h)
 }
